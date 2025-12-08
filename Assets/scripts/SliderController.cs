8275706dbca10f5c9e1002fff607d78c7039eb12
Tijmen-Degr <tpdegroen@gmail.com>
2025-12-08 14:10:21 +0100

    Added progress bar for score tracking
    This progress bar will have a similar functionality as the progress bar from Just Dance where every correct dance move will make the score and therefore also the progress go up and every wrong move makes it go down.

---
+++ Assets/scripts/SliderController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public Text valueText;
+    int progress = 0;
+    public Slider slider;
+
+    public void OnSliderChanged(float value) { 
+        valueText.text = value.ToString();
+    }
+
+    public void UpdateProgress() {
+        progress ++ ;
+        slider.value = progress;
+    }
+
+    public void RemoveProgress()
+    {
+        progress--;
+        slider.value = progress;
+    }
+}
