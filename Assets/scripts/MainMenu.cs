fba1c0bcdf8b4e34eb15eb3d8e01cb09e2f178e7
Tijmen-Degr <tpdegroen@gmail.com>
2025-12-18 14:40:33 +0100

    Functional main menu
    The main menu now directs to the clubscene_ui when you click on the start button. the quit button doesn't work for some reason but I will ask the teachers about that after the break

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
+    public void LoadScene(string sceneName)
     {
-        LevelManager.Instance.LoadScene("ClubScene_UI","CrossFade");
+        SceneManager.LoadScene(sceneName);
     }
 
     public void Settings()
     {
-        LevelManager.Instance.LoadScene("Game", "CrossFade");
+        
     }
 
     public void Quit()
